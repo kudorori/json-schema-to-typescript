e605c1f86f71e76df7802d67bae226f2adfa88c6
Kenny <kudokawai@gmail.com>
2019-12-06 11:46:08 +0800

    Remove UpperCase

---
+++ dist/test/e2e/strictIndexSignatures.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.input = {
+    title: 'StrictIndexSignatures',
+    type: 'object',
+    properties: {
+        maybe: {
+            type: 'string'
+        }
+    },
+    additionalProperties: {
+        type: 'string'
+    }
+};
+exports.options = {
+    strictIndexSignatures: true
+};
+//# sourceMappingURL=strictIndexSignatures.js.map
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import {whiteBright} from 'cli-color'
-import {deburr, isPlainObject, mapValues, trim, upperFirst} from 'lodash'
+import {deburr, isPlainObject, mapValues, upperFirst} from 'lodash'
 import {basename, extname} from 'path'
 import {JSONSchema} from './types/JSONSchema'
 
@@ -168,14 +168,6 @@ export function toSafeString(string: string) {
     deburr(string)
       // replace chars which are not valid for typescript identifiers with whitespace
       .replace(/(^\s*[^a-zA-Z_$])|([^a-zA-Z_$\d])/g, ' ')
-      // uppercase leading underscores followed by lowercase
-      .replace(/^_[a-z]/g, match => match.toUpperCase())
-      // remove non-leading underscores followed by lowercase (convert snake_case)
-      .replace(/_[a-z]/g, match => match.substr(1, match.length).toUpperCase())
-      // uppercase letters after digits, dollars
-      .replace(/([\d$]+[a-zA-Z])/g, match => match.toUpperCase())
-      // uppercase first letter after whitespace
-      .replace(/\s+([a-zA-Z])/g, match => trim(match.toUpperCase()))
       // remove remaining whitespace
       .replace(/\s/g, '')
   )
